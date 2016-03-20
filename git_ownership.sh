0d89105e56e808b4849854b2d07304d4fe58165c
Dom Williams <me@domwillia.ms>
2016-03-20 22:52:19 +0000

    Added option to print full file names

--- git_ownership.sh
+++ git_ownership.sh
@@ -2,8 +2,22 @@
 #!/bin/sh
 set -e
 
+full_name=false
+
+while getopts "f" opt; do
+  case $opt in
+    f)
+      full_name=true
+  esac
+done
+
 for f in `git ls-files | grep "java$"`; do
-  echo `basename $f`
+  if [ "$full_name" = true ]; then
+    echo "$f"
+  else
+    echo `basename $f`
+  fi
+
   git blame --minimal --line-porcelain -w $f | pcregrep -o1 "^author\s(.+)$" | sort | uniq -c | sort -nr
   echo
 done

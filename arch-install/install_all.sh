03a3016d672f4381116bd6a170db2fc3a2093af6
Dom Williams <me@domwillia.ms>
2016-04-13 14:31:14 +0100

    Few fixes to install script

--- arch-install/install_all.sh
+++ arch-install/install_all.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-sudo pacman -S git
-sudo pacman -S zsh
+if [[ $EUID -ne 0 ]]; then
+	echo Must be root
+	exit 1
+fi
+
+pacman -S zsh
 ./install_yaourt.sh
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sudo sh -c "$(wget https://raw.githubusercontent.com/DomWilliams0/wallpaper-fetcher/master/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/DomWilliams0/wallpaper-fetcher/master/install.sh -O -)"
 
-sudo pacman-key --recv-keys AE6866C7962DDE58
-sudo pacman-key --refresh-keys
+pacman-key --recv-keys AE6866C7962DDE58 962DDE58
+pacman-key --lsign-key 962DDE58
+echo -e "[infinality-bundle]\nServer = http://bohoomil.com/repo/\$arch" >> /etc/pacman.conf
+echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman -Syyu
+pacman -S infinality-bundle
+pacman-key --refresh-keys
 
-mkdir ~/.screenshots
+mkdir ~/screenshots
+
+pacman -R vi
+systemctl enable --now NetworkManager.service
+
+mkdir ~/Pictures
+wget https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-268242.jpg -O ~/Pictures/wallpaper.jpg

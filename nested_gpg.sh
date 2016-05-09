72dbd8e2284a89b0396ae69e6d8d95ddfd29d3a4
Dom Williams <me@domwillia.ms>
2016-05-09 23:46:47 +0100

    Add nested gpg script

---
+++ nested_gpg.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if (($# != 4)); then
+	echo "Usage: $0 <file> <output-file> <recipient|-d> <count>"
+	exit 1
+fi
+
+
+file="$1"
+outfile="$2"
+recp="$3"
+n="$4"
+
+do_op() {
+	if [[ "$recp" = "-d" ]]; then
+		gpg --decrypt --yes -r $recp --output $2 $1
+	else
+		gpg --encrypt --sign --yes -r $recp --output $2 $1
+	fi;
+
+	n=$((n-1))
+	echo "$1 => $2 | ($n) remaining"
+}
+
+input=$(mktemp)
+output=$(mktemp)
+
+do_op $file $input
+
+while (($n > 0)); do
+	do_op $input $output
+
+	tmp=$input
+	input=$output
+	output=$tmp
+done;
+
+mv "$input" "$outfile"
+rm -f "$input" "$output"
+echo Output to "$outfile"

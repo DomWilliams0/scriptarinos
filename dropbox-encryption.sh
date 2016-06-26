7cc28a2f0fbbdf52d03ff4f26b95b4df3f986722
Dom Williams <me@domwillia.ms>
2016-06-26 23:01:07 +0100

    Create container with mount command

--- dropbox-encryption.sh
+++ dropbox-encryption.sh
@@ -6,6 +6,10 @@ CONTAINER_PATH="$ROOT/container"
 SYNCED_CONTAINER_PATH="$DROPBOX_DIR/container"
 MOUNT="$HOME/dropbox-container"
 
+CONTAINER_SIZE="1G"
+CONTAINER_DEVICE="dropbox-encrypted-$(whoami)"
+CONTAINER_DEVICE_MAPPED=/dev/mapper/$CONTAINER_DEVICE
+
 HOME=$ROOT
 
 # TODO
@@ -33,10 +37,38 @@ container_synced() {
 }
 
 create_container() {
-	# TODO create
-	# this is just all todos so far isnt it
-	echo creating
-	:
+	set -e
+
+	echo Creating sparse container of size $CONTAINER_SIZE
+	dd if=/dev/zero of=$CONTAINER_PATH bs=1 count=0 seek=1G
+
+	echo Creating LUKS container
+	sudo cryptsetup -y luksFormat $CONTAINER_PATH
+
+	mount_container
+
+	echo Creating filesystem
+	sudo mkfs.ext4 $CONTAINER_DEVICE_MAPPED
+
+	set +e
+}
+
+mount_container() {
+	set -e
+
+	if ! grep "is active." <(sudo cryptsetup status $CONTAINER_DEVICE_MAPPED); then
+		echo Unlocking container
+		sudo cryptsetup luksOpen $CONTAINER_PATH $CONTAINER_DEVICE
+	fi
+
+	if ! grep $MOUNT <(mount); then
+		echo Mounting volume
+		mkdir -p -- $MOUNT
+		sudo mount $CONTAINER_DEVICE_MAPPED $MOUNT
+		sudo chown $(whoami) $MOUNT
+	fi
+
+	set +e
 }
 
 # returns 0 if synced and ready to use, otherwise 1
@@ -51,22 +83,15 @@ prepare_container() {
 
 # exits with failure if container is not yet ready/synced
 wait_for_container() {
-	prepare_container || exit 1 # not ready yet
+	if ! prepare_container; then
+		echo The container is not ready yet
+		exit 1
+	fi
 }
 
 do_mount() {
 	wait_for_container
-
-	# create container if doesn't already exist
-	#	create sparse dd
-	#	create loopback device
-	#	encrypt with cryptsetup
-	#	create fs
-	# otherwise sync with dropbox
-	#	wait for dropbox to finish syncing it
-	# create loopback device
-	# mount in $MOUNT
-}
+	mount_container
 
 do_unmount() {
 	echo unmount

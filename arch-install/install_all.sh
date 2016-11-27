0ff839c27b37bf87ff2e4e0ae2178ac82f09ddd3
Dom Williams <me@domwillia.ms>
2016-11-27 16:42:18 +0100

    Remove things from (pretty much useless anyway) arch install script

--- arch-install/install_all.sh
+++ arch-install/install_all.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S zsh
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 pacman-key --recv-keys AE6866C7962DDE58 962DDE58
 pacman-key --lsign-key 962DDE58
 echo -e "[infinality-bundle]\nServer = http://bohoomil.com/repo/\$arch" >> /etc/pacman.conf
@@ -13,6 +9,3 @@ sudo pacman -S infinality-bundle
 sudo pacman-key --refresh-keys
 
 mkdir ~/screenshots
-
-sudo pacman -R vi
-sudo systemctl enable --now NetworkManager.service
